223ca96ff2b704f113191780c34fa7cafb0807d4
swapnil <bhalerao.swapnil11@gmail.com>
2021-09-18 15:12:26 +0530

    1: Inorder, Preorder and Postorder

--- tree/main.cpp
+++ tree/main.cpp
@@ -6,9 +6,92 @@
 //
 
 #include <iostream>
+using namespace std;
+struct Node
+{
+    Node *left;
+    Node *right;
+    int data;
+    Node() : left(nullptr), right(nullptr), data(0) {}
+    Node(int val) : left(nullptr), right(nullptr), data(val) {}
+};
 
-int main(int argc, const char * argv[]) {
+class Tree
+{
+private:
+    Node *mpRoot;
+    void createTree();
+
+public:
+    Tree();
+    Node *getRoot() { return mpRoot; }
+    void preOrder(Node *);
+    void postOrder(Node *);
+    void inOrder(Node *);
+};
+Tree::Tree() : mpRoot(nullptr)
+{
+    createTree();
+}
+void Tree::createTree()
+{
+    mpRoot = new Node(10);
+    mpRoot->left = new Node(2);
+    mpRoot->left->left = new Node(6);
+
+    mpRoot->right = new Node(5);
+    mpRoot->right->left = new Node(8);
+    mpRoot->right->right = new Node(3);
+}
+void Tree::inOrder(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+void Tree::preOrder(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    inOrder(root->left);
+    inOrder(root->right);
+}
+
+void Tree::postOrder(Node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    inOrder(root->left);
+    inOrder(root->right);
+    cout << root->data << " ";
+}
+
+int main(int argc, const char *argv[])
+{
     // insert code here...
     std::cout << "Hello, World!\n";
+    Tree t;
+    std::cout << "In-order => ";
+    t.inOrder(t.getRoot());
+    cout << endl;
+    
+    std::cout << "Pre-order => ";
+    t.preOrder(t.getRoot());
+    cout << endl;
+    
+    std::cout << "Post-order => ";
+    t.postOrder(t.getRoot());
+    cout << endl;
+
     return 0;
 }
