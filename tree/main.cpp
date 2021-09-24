7e899dc6bf06e7a96d418cda4d4c0295f3ba60f5
swapnil <bhalerao.swapnil11@gmail.com>
2021-09-24 16:00:26 +0530

    4: Size of Binary Tree

--- tree/main.cpp
+++ tree/main.cpp
@@ -3,7 +3,7 @@
 //  tree
 //
 //  Created by Swapnil Bhalerao on 18/09/21.
-//  Question: Same Tree
+//  Question: Size of Binary Tree
 
 #include <iostream>
 using namespace std;
@@ -21,18 +21,18 @@ class Tree
 {
 private:
     Node *mpRoot;
+
 public:
     Tree();
     Node *getRoot() { return mpRoot; }
     void inOrder(Node *);
-    bool sameTree(Node *, Node *);
-    void createTree1();
-    void createTree2();
+    void createTree();
+    int sizeofTree(Node *);
 };
 Tree::Tree() : mpRoot(nullptr)
 {
 }
-void Tree::createTree1()
+void Tree::createTree()
 {
     mpRoot = new Node(10);
     mpRoot->left = new Node(-5);
@@ -43,29 +43,7 @@ void Tree::createTree1()
     mpRoot->right = new Node(30);
     mpRoot->right->right = new Node(36);
 }
-void Tree::createTree2()
-{
-//    create not same tree
-//    mpRoot = new Node(10);
-//    mpRoot->left = new Node(-5);
-//    mpRoot->left->left = new Node(-10);
-//    mpRoot->left->right = new Node(0);
-//    mpRoot->left->right->right = new Node(5);
-//
-//    mpRoot->right = new Node(30);
-//    mpRoot->right->left = new Node(15);
-//    mpRoot->right->right = new Node(36);
-    
-    mpRoot = new Node(10);
-    mpRoot->left = new Node(-5);
-    mpRoot->left->left = new Node(-10);
-    mpRoot->left->right = new Node(0);
-    mpRoot->left->right->right = new Node(5);
 
-    mpRoot->right = new Node(30);
-    mpRoot->right->right = new Node(36);
-    
-}
 void Tree::inOrder(Node *root)
 {
     if (root == nullptr)
@@ -76,40 +54,25 @@ void Tree::inOrder(Node *root)
     cout << root->data << " ";
     inOrder(root->right);
 }
-
-bool Tree::sameTree(Node *root1, Node *root2)
+int Tree::sizeofTree(Node *root)
 {
-    if (root1 == nullptr && root2 == nullptr)
+    if (root == nullptr)
     {
-        return true;
+        return 0;
     }
-    if (root1 == nullptr || root2 == nullptr)
-    {
-        return false;
-    }
-
-    return (root1->data == root2->data) && sameTree(root1->left, root2->left) && sameTree(root1->right, root2->right);
+    return 1 + sizeofTree(root->left) + sizeofTree(root->right);
 }
 int main(int argc, const char *argv[])
 {
     // insert code here...
     std::cout << "Compare Two Tree\n";
-    Tree t1, t2;
-    t1.createTree1();
-    t2.createTree2();
+    Tree t;
+    t.createTree();
+
     std::cout << "In-order => ";
-    t1.inOrder(t1.getRoot());
-    std::cout << "\nIn-order => ";
-    t2.inOrder(t2.getRoot());
+    t.inOrder(t.getRoot());
     cout << endl;
-    if (t1.sameTree(t1.getRoot(), t2.getRoot()))
-    {
-        cout << "Both Tree's are same" << endl;
-    }
-    else
-    {
-        cout << "Tree's are not same" << endl;
-    }
+    cout << "Size of Tree => " << t.sizeofTree(t.getRoot()) << endl;
 
     return 0;
 }
