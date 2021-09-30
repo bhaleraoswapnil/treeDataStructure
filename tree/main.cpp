09c913bfd4dfcebc38cad67fd0e41cf7e77b095c
swapnil <bhalerao.swapnil11@gmail.com>
2021-09-30 08:24:16 +0530

    12: Lowest common Ancester in Binary Tree.

--- tree/main.cpp
+++ tree/main.cpp
@@ -3,11 +3,9 @@
 //  tree
 //
 //  Created by Swapnil Bhalerao on 18/09/21.
-//  Question: Lowest common Ancester in Binary Search Tree (BST).
+//  Question: Lowest common Ancester in Binary Tree.
 
 #include <iostream>
-#include <queue>
-#include <stack>
 using namespace std;
 
 struct Node
@@ -37,13 +35,15 @@ Tree::Tree() : mpRoot(nullptr)
 }
 void Tree::createTree()
 {
-    mpRoot = new Node(10);
-    mpRoot->left = new Node(-5);
-    mpRoot->left->left = new Node(-10);
-    mpRoot->left->right = new Node(0);
-    mpRoot->left->right->right = new Node(5);
-    mpRoot->right = new Node(30);
-    mpRoot->right->right = new Node(36);
+    mpRoot = new Node(1);
+
+    mpRoot->left = new Node(2);
+    mpRoot->left->left = new Node(4);
+    mpRoot->left->right = new Node(5);
+
+    mpRoot->right = new Node(3);
+    mpRoot->right->left = new Node(6);
+    mpRoot->right->right = new Node(7);
 }
 
 void Tree::inOrder(Node *root)
@@ -63,18 +63,21 @@ Node *Tree::lowestCommonAncester(Node *root, int a, int b)
     {
         return nullptr;
     }
-    if (root->data > a && root->data > b)
-    {
-        return lowestCommonAncester(root->left, a, b);
-    }
-    else if (root->data < a && root->data < b)
-    {
-        return lowestCommonAncester(root->right, a, b);
-    }
-    else
+
+    if (root->data == a || root->data == b)
     {
         return root;
     }
+
+    Node *leftSearch = lowestCommonAncester(root->left, a, b);
+    Node *rightSearch = lowestCommonAncester(root->right, a, b);
+
+    if (leftSearch != nullptr && rightSearch != nullptr)
+    {
+        return root;
+    }
+
+    return leftSearch != nullptr ? leftSearch : rightSearch;
 }
 
 int main(int argc, const char *argv[])
